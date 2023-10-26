e72b434e482bd9f10652554f04ce60a32d8607bc
Matvey Gasnikov <gasnikov.ma@gmail.com>
2023-10-26 20:44:43 +0300

    hw2_final

--- src/test/java/edu/hw2/Task2Test.java
+++ src/test/java/edu/hw2/Task2Test.java
@@ -1,26 +1,40 @@
 package edu.hw2;
 
-import edu.hw2.task2.Rectangle;
-import edu.hw2.task2.Square;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task2Test {
-    @Test
-    @DisplayName("площадь прямоугольника")
-    void Area_ShouldReturnTwelve() {
-        Rectangle rect = new Rectangle(10, 20);
-        int ans = rect.setWidth(4).setHeight(3).area();
-        assertThat(ans).isEqualTo(12);
+    @ParameterizedTest
+    @MethodSource("providerForSquare")
+    void area_ShouldReturnEquals(Task2.Shape shape) {
+        assertEquals(shape.area(), 400);
+
     }
 
     @Test
     @DisplayName("площадь квадрата")
-    void Area_ShouldReturnEight() {
-        Rectangle square = new Square(10);
-        int ans = square.setHeight(2).setWidth(4).area();
-        assertThat(ans).isEqualTo(8);
+    void area_Square() {
+        Task2.Square square = new Task2.Square(31);
+        assertEquals(square.area(), 961);
+    }
+
+    @Test
+    @DisplayName("площадь прямоугольника")
+    void area_Rectangle() {
+        Task2.Rectangle square = new Task2.Rectangle(10, 41);
+        assertEquals(square.area(), 410);
+    }
+
+    private static Stream<Arguments> providerForSquare() {
+        return Stream.of(
+            Arguments.of(new Task2.Rectangle(40, 10)),
+            Arguments.of(new Task2.Square(20))
+        );
 
     }
 
