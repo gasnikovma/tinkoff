533593ac9d22ffeed0ed7918b7d210b64ee0d8c2
Matvey Gasnikov <gasnikov.ma@gmail.com>
2023-10-28 22:57:24 +0300

    hw3_final

---
+++ src/test/java/edu/hw1/Task4Test.java
@@ -0,0 +1,19 @@
+package edu.hw1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class Task4Test {
+
+    @ParameterizedTest
+    @CsvSource({
+        "123456,  214365",
+        "hTsii  s aimex dpus rtni.g, This is a mixed up string.",
+        "badce, abcde"
+    })
+    void fixString_ShouldReturnValue(String input, String res) {
+        assertThat(Task4.fixString(input)).isEqualTo(res);
+    }
+
+}
