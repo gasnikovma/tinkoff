b604ec4d1c5aeaf9205fa00df9a49cdf87dbbdc5
Matvey Gasnikov <gasnikov.ma@gmail.com>
2023-12-07 16:54:07 +0300

    hw9

---
+++ src/main/java/edu/hw9/task1/Metric.java
@@ -0,0 +1,6 @@
+package edu.hw9.task1;
+
+public record Metric(String name, double sum, double average, double min, double max) {
+
+}
+
---
+++ src/main/java/edu/hw9/task1/MetricType.java
@@ -0,0 +1,5 @@
+package edu.hw9.task1;
+
+public record MetricType(String name, double[] val) {
+
+}
